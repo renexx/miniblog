02e2c00b4999b40d4fa693ed1935fd175da56a6e
renexx <rbolf.rene@gmail.com>
2020-08-11 23:04:50 +0200

    add some css code for better look

--- blog/resources/views/master.blade.php
+++ blog/resources/views/master.blade.php
@@ -12,7 +12,7 @@
 
     </head>
     <body>
-        <header class="container">
+        <header class="container-fluid">
             @if(count($errors) > 0)
                 <ul class="alert alert-danger">
                     @foreach ($errors->all() as $error)
@@ -23,7 +23,7 @@
 
             @if(Auth::check())
 
-                <nav class="navigation">
+                <!-- <nav class="navigation">
                     <div class="btn-group btn-group-sm pull-left">
                         <a href="{{url('/stat')}} " class="btn btn-default">Štatistiky</a>
                         <a href="{{url('/post')}} " class="btn btn-default">Všetky odkazy</a>
@@ -33,6 +33,23 @@
                         <span class="username">{{ Auth::user()->name}}</span>
                          <a href="{{url('/logout')}}" class="btn btn-default logout"> logout</a>
                     </div>
+                </nav> -->
+
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                  <a class="navbar-brand" href="#">Odkazy</a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                      <a class="nav-item nav-link active" href="{{url('/stat')}}">Štatistiky</a>
+                      <a class="nav-item nav-link" href="{{url('/post')}}">Všetky odkazy</a>
+                      <a class="nav-item nav-link" href="{{url('user/' . Auth::id())}}">Moje odkazy</a>
+                      <a class="nav-item nav-link" href="{{url('user/' . Auth::id())}}">{{ Auth::user()->name}}</a>
+
+                      <a class="nav-item nav-link disabled" href="{{url('/logout')}}">Logout</a>
+                    </div>
+                  </div>
                 </nav>
             @else
             <div class="btn-group btn-group-sm pull-left">
--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -7,7 +7,7 @@
      <h1 class="box-heading text-muted">
          Odkazy
      </h1>
-     
+
 
      <form action="{{ route('post.store') }}" method="POST">
          @csrf
@@ -16,11 +16,11 @@
              <label for="content">Odkaz</label>
              <textarea name="content"
                        id="content"
-                       class="form-control" 
+                       class="form-control"
                        placeholder="tu môžete pisat"
                        rows="3">{{ old('content') }}</textarea>
          </div>
- 
+
          <button type="submit" class="btn btn-primary">Odoslať</button>
      </form>
 
@@ -28,22 +28,23 @@
 
         <article id="post-{{$post->id }}" class="post">
             <header class="post-header">
-               
-                    <time>
-                        <small>Date: {{$post->created_at}}</small>
-                    </time>
-                
+
+
+
             </header>
             <div class="post-content"></div>
                 <p>
                     {{$post->content}}
                 </p>
                 <p class="written-by small">
-                    <small> - writte by
+                    <small> - napísal
                         <a href="{{ url('user',$post->user->id)}}">
                             {{$post->user->name}}
                         </a>
                     </small>
+                    <time>
+                        <small>Dátum: {{$post->created_at}}</small>
+                    </time>
                 </p>
             <footer class="post-footer">
 
